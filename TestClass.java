7e0354d4763c2b14da1f99671f64d96dc2cab61a
= <masunawin@gmail.com>
2020-05-09 06:24:35 +0530

    commit 4

--- TestClass.java
+++ TestClass.java
@@ -5,6 +5,7 @@ class TestClass
 	{
 		System.out.println("Masuna Naveen Kumar");
 		System.out.println("1");
-		System.out.println("3");
-	}
+		System.out.println("3");	
+		System.out.println("4");
+	}	
 }
