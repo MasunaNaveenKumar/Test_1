fea706285456aa15f41fc9e25c95949bb5b9d0c9
MasunaNaveenKumar <64964628+MasunaNaveenKumar@users.noreply.github.com>
2020-05-09 06:30:48 +0530

    commit delete 4

--- TestClass.java
+++ TestClass.java
@@ -5,7 +5,6 @@ class TestClass
 	{
 		System.out.println("Masuna Naveen Kumar");
 		System.out.println("1");
-		System.out.println("3");	
-		System.out.println("4");
+		System.out.println("3");
 	}	
 }
